23045f2a3af82867e9eef94e0994ef297681bf61
elena-buscaroli <ele.buscaroli@gmail.com>
2021-02-28 19:41:50 +0100

    updates

--- c++/main.cpp
+++ c++/main.cpp
@@ -29,111 +29,107 @@ int main(){
 	std::cout << "\ntree -> " << tree << std::endl;
 
 
+	// FIND
+	std::cout << "\nFind function\n"
+						<< "tree before find() -> " << tree << '\n'
+						<< "find(3) - present -> " << (*tree.find(3)).first << '\n'
+						<< "find(25) - not present -> " << &(*tree.find(25)) << '\n'
+						<< "tree.end() -> " << &(*tree.end()) << std::endl;
+
+
 	// EMPLACE
-	std::cout << "\nEmplace function\n";
-	std::cout << "tree before emplace() -> " << tree << std::endl;
+	std::cout << "\nEmplace function\n"
+						<< "tree before emplace() -> " << tree << '\n';
 	tree.emplace(2, 2);
-	std::cout << "emplace(2, 2) -> " << tree << std::endl;
-	std::cout << "tree.find(2) -> (" << (*tree.find(2)).first << ", " << (*tree.find(2)).second << ")" << std::endl;
+	std::cout << "emplace(2, 2) -> " << tree << '\n'
+						<< "tree.find(2) -> (" << (*tree.find(2)).first << ", " << (*tree.find(2)).second << ")" << '\n';
 
 	int l{9};
 	tree.emplace(l, 2);
-	std::cout << "emplace(9, 2) -> " << tree << std::endl;
-	std::cout << "find(9) -> (" << (*tree.find(9)).first << ", " << (*tree.find(9)).second << ")" << std::endl;
+	std::cout << "emplace(9, 2) -> " << tree << '\n'
+						<< "find(9) -> (" << (*tree.find(9)).first << ", " << (*tree.find(9)).second << ")" << '\n';
 
 	tree.emplace(std::make_pair(15,7));
-	std::cout << "emplace(15, 7) -> " << tree << std::endl;
-	std::cout << "tree.find(15) -> (" << (*tree.find(15)).first << ", " << (*tree.find(15)).second << ")" << std::endl;
+	std::cout << "emplace(15, 7) -> " << tree << '\n'
+						<< "tree.find(15) -> (" << (*tree.find(15)).first << ", " << (*tree.find(15)).second << ")" << std::endl;
 
 
 	// INSERT
-	std::cout << "\nInsert function\n";
-	std::cout << "tree before insert() -> " << tree << std::endl;
-
-	std::cout << "boolean output of insert(11, 12) -> " << tree.insert(std::pair<int, int> {11, 12}).second << std::endl;
-	std::cout << "tree after insert(11, 12) -> " << tree << std::endl;
-
-	std::cout << "boolean output of insert(3, 12) - already present -> " << tree.insert(std::pair<int, int> {3, 12}).second << std::endl; 
-	std::cout << "tree after inserting (3, 12) -> " << tree << std::endl;
-
-
-	// FIND
-	std::cout << "\nFind function\n";
-	std::cout << "tree before find() -> " << tree << std::endl;
-
-	std::cout << "find(3) - present -> " << (*tree.find(3)).first << std::endl;
-	std::cout << "find(25) - not present -> " << &(*tree.find(25)) << std::endl;
-	std::cout << "tree.end() -> " << &(*tree.end()) << std::endl;
+	std::cout << "\nInsert function\n"
+						<< "tree before insert() -> " << tree << '\n'
+						<< "boolean output of insert(11, 12) -> " << tree.insert(std::pair<int, int> {11, 12}).second << '\n'
+						<< "tree after insert(11, 12) -> " << tree << '\n'
+						<< "boolean output of insert(3, 12) - already present -> " << tree.insert(std::pair<int, int> {3, 12}).second << '\n'
+						<< "tree after inserting (3, 12) -> " << tree << std::endl;
 
 
 	// SUBSCRIPTING 
-	std::cout << "\nSubscripting operator\n";
-	std::cout << "tree before subscripting -> " << tree << std::endl;
-
-	std::cout << "tree[5] - present -> " << tree[5] << std::endl;
-	std::cout << "tree[17] - not present -> " << tree[17] << std::endl; 
+	std::cout << "\nSubscripting operator\n"
+						<< "tree before subscripting -> " << tree << '\n'
+						<< "tree[5] - present -> " << tree[5] << '\n'
+						<< "tree[17] - not present -> " << tree[17] << '\n';
 	tree[5] = 6;
-	std::cout << "tree[5]=6 -> " << tree[5] << std::endl;
+	std::cout << "tree[5]=6 -> " << tree[5] << '\n';
 
 	tree[17] = 20;
 	std::cout << "tree[17]=20 -> " << tree[17] << std::endl; 
 
 
 	// COPY
-	std::cout << "\nCopy semantics\n";
-	std::cout << "tree before copy -> " << tree << std::endl;
+	std::cout << "\nCopy semantics\n"
+						<< "tree before copy -> " << tree << '\n';
 
 	bst<int,int> t1 = tree;
 	bst<int,int> t2{tree};
-	std::cout << "tree after tree1=tree; tree2{tree} -> " << tree << '\n';
-	std::cout << "tree1=tree -> " << t1 << std::endl;
-	std::cout << "tree2{tree} -> " << t2 << std::endl;
+	std::cout << "tree after tree1=tree; tree2{tree} -> " << tree << '\n'
+						<< "tree1=tree -> " << t1 << '\n'
+						<< "tree2{tree} -> " << t2 << '\n';
 
 	t1.insert(std::pair<int, int> {20,12});
 	t2.insert(std::pair<int, int> {22,12});
 	tree.insert(std::pair<int, int> {16,12});
-	std::cout << "tree.insert({16, 12}) -> " << tree << std::endl;
-	std::cout << "tree1.insert({20, 12}) -> " << t1 << std::endl;
-	std::cout << "tree1.insert({22, 12}) -> " << t2 << std::endl;
+	std::cout << "tree.insert({16, 12}) -> " << tree << '\n'
+						<< "tree1.insert({20, 12}) -> " << t1 << '\n'
+						<< "tree2.insert({22, 12}) -> " << t2 << std::endl;
 
 
 	// ERASE
-	std::cout << "\nErase function\n";
-	std::cout << "tree before erase() -> " << tree << std::endl;
+	std::cout << "\nErase function\n"
+						<< "tree before erase() -> " << tree << '\n';
 	tree.erase(8);
-	std::cout << "erase(8) - root -> " << tree << std::endl;
+	std::cout << "erase(8) - root -> " << tree << '\n';
 	tree.erase(5);
-	std::cout << "erase(5) - leaf node -> " << tree << std::endl;
+	std::cout << "erase(5) - leaf node -> " << tree << '\n';
 	tree.erase(13);
 	std::cout << "erase(13) - with only one child node -> " << tree << std::endl;
 
 
 	// BALANCE
-	std::cout << "\nBalance function\n";
-	std::cout << "tree before balance() -> " << tree << std::endl;
-	std::cout << "before balance: tree.begin() -> " << &(*tree.begin()) << "\ttree.end() -> " << &(*tree.end()) << '\n';
+	std::cout << "\nBalance function\n"
+						<< "tree before balance() -> " << tree << '\n'
+						<< "before balance: tree.begin() -> " << &(*tree.begin()) << "\ttree.end() -> " << &(*tree.end()) << '\n';
 	tree.balance();
-	std::cout << "after balance: tree.begin() -> " << &(*tree.begin()) << "\ttree.end() -> " << &(*tree.end()) << std::endl;
-	std::cout << "tree after balance() -> " << tree << std::endl;
+	std::cout << "after balance: tree.begin() -> " << &(*tree.begin()) << "\ttree.end() -> " << &(*tree.end()) << '\n'
+						<< "tree after balance() -> " << tree << std::endl;
 
 
 	// MOVE
-	std::cout << "\nMove semantics\n";
-	std::cout << "tree before move -> " << tree << std::endl;
+	std::cout << "\nMove semantics\n"
+						<< "tree before move -> " << tree << '\n';
 
 	bst<int,int> tree1{};
 	bst<int,int> tree2{std::move(tree)}; 
-	std::cout << "tree2{std::move(tree)} -> " << tree2 << std::endl;
-	std::cout << "tree after move -> " << tree << std::endl;
+	std::cout << "tree2{std::move(tree)} -> " << tree2 << '\n'
+						<< "tree after move -> " << tree << '\n';
 
 	tree1 = std::move(tree2); 
-	std::cout << "tree1 = std::move(tree2) -> " << tree1 << std::endl;
-	std::cout << "tree2 after move -> " << tree2 << std::endl;
+	std::cout << "tree1 = std::move(tree2) -> " << tree1 << '\n'
+						<< "tree2 after move -> " << tree2 << std::endl;
 
 
 	// CLEAR
-	std::cout << "\nClear function\n";
-	std::cout << "tree1 before clear() -> " << tree1 << std::endl;
+	std::cout << "\nClear function\n"
+						<< "tree1 before clear() -> " << tree1 << '\n';
 	tree1.clear();
 	std::cout << "tree1.clear() -> " << tree1 << std::endl;
 
@@ -141,20 +137,22 @@ int main(){
 	// BEGIN AND END
 	std::cout << "\nBegin and End functions\n";
 	bst<int,int> tree3{};
-	std::cout << "New empty tree -> " << tree3 << std::endl;
-	std::cout << "insert({1, 12}) -> " << &(*tree3.insert(std::pair<int, int> {1,12}).first) << std::endl;
+
+	std::cout << "New empty tree -> " << tree3 << '\n'
+						<< "address of pair{1,12} after insert({1, 12}) -> " << &(*tree3.insert(std::pair<int, int> {1,12}).first) << '\n';
+	
 	tree3.insert(std::pair<int, int> {2,12});
 	tree3.insert(std::pair<int, int> {6,12});
 	tree3.insert(std::pair<int, int> {3,12});
 
-	std::cout << "New tree after adding pairs {2,12}, {6, 12}, {3, 12} -> " << tree3 << std::endl;
-	std::cout << "tree.begin() -> " << &(*tree3.begin()) << '\t';
-	std::cout << "tree.end() -> " << &(*tree3.end()) << std::endl;
+	std::cout << "New tree after adding pairs {2,12}, {6, 12}, {3, 12} -> " << tree3 << '\n'
+						<< "tree.begin() -> " << &(*tree3.begin()) << '\t'
+						<< "tree.end() -> " << &(*tree3.end()) << '\n';
 
 	tree3.erase(1);
-	std::cout << "tree.erase(1) -> " << tree3 << std::endl;
-	std::cout << "tree.begin() -> " << &(*tree3.begin()) << '\t';
-	std::cout << "tree.end() -> " << &(*tree3.end()) << std::endl;
+	std::cout << "tree.erase(1) -> " << tree3 << '\n'
+						<< "tree.begin() -> " << &(*tree3.begin()) << '\t'
+						<< "tree.end() -> " << &(*tree3.end()) << std::endl;
 
 	return 0;
 }
